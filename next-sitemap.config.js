be04da185998ee152faa0ba8909cf5073b31953b
Tinotenda Jecha <tinotendajecha@gmail.com>
2024-02-20 22:50:17 +0200

    Added sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://sovietautoandbody.co.zw/',
+    generateRobotsTxt: true,
+  }
+
+// module.exports = {
+//     siteUrl: 'https://sovietautoandbody.co.zw/',
+//     generateRobotsTxt: true,
+//     sitemapSize: 7000,
+// }
